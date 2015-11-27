0179165c3b550db425ecf776b3d78cb34b638253
uppfinnarn <uppfinnarn@gmail.com>
2015-11-27 01:20:46 +0100

    Re-bind to pjax-loaded play links

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+// Invoke the player from play links
+function playClickHandler() {
+  var playerElement = document.getElementById('player');
+  var player = angular.element(playerElement).scope().player;
+  player.load($(this).data('play'));
+}
+$(document).on('ready pjax:end', function() {
+  $('[data-play]').click(playClickHandler);
+});
+
 $(function() {
   // Enable PJAX loading; allows the player to persist between pages
   $(document).pjax('a', '#content', { fragment: '#content' });
-  
-  // Invoke the player from play links
-  $('[data-play]').click(function() {
-    var playerElement = document.getElementById('player');
-    var player = angular.element(playerElement).scope().player;
-    player.load($(this).data('play'));
-  });
 });
